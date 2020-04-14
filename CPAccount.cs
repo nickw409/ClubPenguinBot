5071f2a9fdd36cfb793cf0524fe97992db5ee9eb
nickw409 <nwiley333@gmail.com>
2020-04-14 15:22:28 -0700

    Added CPAccount and WindowManip classes
    This update allows the bot to control multiple accounts at once. While it mostly works there is a bug where the mouse won't click when trying to move the character.

---
+++ CPAccount.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClubPenguinBot
+{
+    public class CPAccount
+    {
+        public enum Action
+        {
+            bootUp,
+            moveLeft,
+            moveRight,
+            openActionMenu,
+            startMining
+        }
+        public int tab { get; set; }
+        public Action lastAction { get; set; }
+        public Action nextAction { get; set; }
+        public DateTime actionStartTime { get; set; }
+        public Action lastMovement { get; set; }
+        public CPAccount(int Tab)
+        {
+            tab = Tab;
+            lastAction = Action.bootUp;
+            nextAction = Action.moveLeft;
+            lastMovement = Action.bootUp;
+            actionStartTime = DateTime.Now;
+        }
+
+        public bool isReady()
+        {
+            DateTime currentTime = DateTime.Now;
+            switch (lastAction)
+            {
+                case Action.bootUp:
+                    {
+                        return true;
+                    }
+                case Action.moveLeft:
+                    {
+                        TimeSpan t = currentTime - actionStartTime;
+                        if (t.TotalMilliseconds >= 0.0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case Action.moveRight:
+                    {
+                        TimeSpan t = currentTime - actionStartTime;
+                        if (t.TotalMilliseconds >= 0.0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case Action.openActionMenu:
+                    {
+                        TimeSpan t = currentTime - actionStartTime;
+                        if (t.TotalMilliseconds >= 0.0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case Action.startMining:
+                    {
+                        TimeSpan t = currentTime - actionStartTime;
+                        if (t.TotalMilliseconds >= 13000.0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+
+        public void runNextAction()
+        {
+            if (isReady())
+            {
+                switch (nextAction)
+                {
+                    case Action.moveLeft:
+                        {
+                            WindowManip.openTab(tab);
+                            System.Threading.Thread.Sleep(900);
+                            WindowManip.moveLeft();
+                            System.Threading.Thread.Sleep(800);
+                            WindowManip.openActionMenu();
+                            System.Threading.Thread.Sleep(300);
+                            WindowManip.startMining();
+                            System.Threading.Thread.Sleep(300);
+                            actionStartTime = DateTime.Now;
+                            lastAction = Action.startMining;
+                            nextAction = Action.moveRight;
+                            lastMovement = Action.moveLeft;
+                            break;
+                        }
+                    case Action.moveRight:
+                        {
+                            WindowManip.openTab(tab);
+                            System.Threading.Thread.Sleep(900);
+                            WindowManip.moveRight();
+                            System.Threading.Thread.Sleep(800);
+                            WindowManip.openActionMenu();
+                            System.Threading.Thread.Sleep(300);
+                            WindowManip.startMining();
+                            System.Threading.Thread.Sleep(300);
+                            actionStartTime = DateTime.Now;
+                            lastAction = Action.startMining;
+                            nextAction = Action.moveLeft;
+                            lastMovement = Action.moveRight;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ WindowManip.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace ClubPenguinBot
+{
+    public class WindowManip
+    {
+        public const int TAB_WIDTH = 240;
+        public const int TAB_HEIGHT = 12;
+        public const int INPUT_MOUSE = 0;
+        public const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        public const int MOUSEEVENTF_MOVE = 0x0001;
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        public const int MOUSEEVENTF_LEFTUP = 0x0004;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        public const int MOUSEEVENTF_RIGHTUP = 0x0010;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint cInputs, Input[] inputs, int size);
+        //[DllImport("user32.dll", SetLastError = true)]
+        //private static extern bool GetCursorPos(out Point point);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        internal struct MouseInput
+        {
+            public int X;
+            public int Y;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        internal struct Input
+        {
+            public int type;
+            public MouseInput MouseInput;
+        }
+
+        public static void openTab(int tab)
+        {
+            int mouseX = 120 + (tab * TAB_WIDTH);
+            mouseClick(mouseX, TAB_HEIGHT);
+        }
+        static void mouseClick(int mouseX, int mouseY)
+        {
+            Input[] inputs = new Input[2];
+
+            SetCursorPos(mouseX, mouseY);
+
+            inputs[0] = new Input();
+            inputs[0].type = INPUT_MOUSE;
+            inputs[0].MouseInput.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+            inputs[1] = new Input();
+            inputs[1].type = INPUT_MOUSE;
+            inputs[1].MouseInput.dwFlags = MOUSEEVENTF_LEFTUP;
+
+            uint result = SendInput(2, inputs, Marshal.SizeOf(typeof(Input)));
+            if (result == 0)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+        public static void moveLeft()
+        {
+            mouseClick(792, 978);
+        }
+        public static void moveRight()
+        {
+            mouseClick(954, 978);
+        }
+        public static void openActionMenu()
+        {
+            mouseClick(778, 1313);
+        }
+        public static void startMining()
+        {
+            mouseClick(772, 838);
+        }
+    }
+}
